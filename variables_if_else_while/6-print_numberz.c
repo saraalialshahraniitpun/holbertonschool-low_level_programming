5ead21bc5cc4abbffc773c885ac8797a171f5c7f
root <root@ip-10-42-36-126.ec2.internal>
2026-07-06 10:53:52 +0000

    Task 6 / fixed

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0,
+ * using int instead of char, and putchar only twice.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 48;
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+
+	return(0);
+}
