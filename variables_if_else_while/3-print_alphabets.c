a96f645832bd826f9d19d48107c027d43f7f8f7e
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-06 13:25:14 +0300

    Print lowercase and uppercase alphabets
    Refactored the code to print uppercase alphabets after lowercase, using putchar only three times.

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,26 +4,28 @@
  * main - Entry point
  *
  * Description: Prints the alphabet in lowercase, and then in uppercase,
- * followed by a new line, using putchar only thrice.
+ * followed by a new line.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char ch;
-	ch ='a';
+
+	ch = 'a';
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	 ch ='A';
-        while (ch <= 'Z')
-        {
-                putchar(ch);
-                ch++;
-        }
+
+	ch = 'A';
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
-
