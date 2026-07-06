8aefef07ba064824db4649c1425308f22fe70f11
root <root@ip-10-42-36-126.ec2.internal>
2026-07-06 10:29:21 +0000

    Task 2:I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game / fixed

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, followed by a new line,
+ * using putchar only twice.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
