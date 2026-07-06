cb6cf81325a262a9d2e91e930fbb4e9ba975e2d1
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-06 14:06:04 +0300

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	char ch;
+
 	ch = '0';
 	while (ch <= '9')
 	{
@@ -17,14 +18,14 @@ int main(void)
 		ch++;
 	}
 
-	 ch = 'a';
-        while (ch <= 'f')
-        {
-                putchar(ch);
-                ch++;
-        }
+	ch = 'a';
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
 
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
