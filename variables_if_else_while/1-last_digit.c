f39795a4103834b62039bdffa82fd6c106aecb65
root <root@ip-10-42-36-126.ec2.internal>
2026-07-06 10:11:03 +0000

    Task 1: The last digit / fixed

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a randomly generated number
+ * and determines its properties.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_d;
+
+	srand(time(NULL));
+	n = rand() - RAND_MAX / 2;
+
+	last_d = n % 10;
+
+	if (last_d > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
+	}
+	else if (last_d == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_d);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
+	}
+
+	return (0);
+}
