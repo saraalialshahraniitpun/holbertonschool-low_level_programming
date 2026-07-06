5e77385d297d33fba69ad32fa8c264d286b84887
root <root@ip-10-42-36-126.ec2.internal>
2026-07-06 10:59:15 +0000

    Task 7 / fixed

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	
+	ch ='z';
+	while ( ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+
+	putchar('\n');
+
+	return(0);
+}
