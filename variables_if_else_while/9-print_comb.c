b7c34e968064951c25e62c4e438f77f71eb249f5
root <root@ip-10-42-36-126.ec2.internal>
2026-07-06 11:09:49 +0000

    Task 9  / fixed

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+	int num;
+
+	num = 48;
+	while (num <= 57)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
