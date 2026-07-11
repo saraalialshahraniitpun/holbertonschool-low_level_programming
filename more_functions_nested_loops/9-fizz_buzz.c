da386cb2c94acabf943f2da6c714e1ba9b022c7d
saraalialshahraniitpun <root@ip-10-42-231-65.ec2.internal>
2026-07-11 14:24:50 +0000

    Add 9-fizz_buzz.c standard interview solution

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 1 to 100, followed by a new line.
+ * For multiples of three, prints Fizz instead of the number.
+ * For multiples of five, prints Buzz.
+ * For numbers which are multiples of both three and five, prints FizzBuzz.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
