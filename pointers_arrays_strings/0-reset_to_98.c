c5d41cf540f446c4c125610350604e9b68a27f55
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-12 08:44:12 +0000

    Fix task 0 with Betty style compliance

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of the integer it points to to 98
+ * @n: A pointer to an int to be updated
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
