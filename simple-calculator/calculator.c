eb60854cb6ebfeaca2205d9f46be2f48b6474df5
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-14 06:59:35 +0000

    fixed

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -7,6 +7,20 @@
  */
 int main(void)
 {
+	int choice = -1;
+
+	do {
+		printf("Simple Calculator\n");
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+
+		printf("Choice: ");
+
+		if (scanf("%d", &choice) == 1)
+		{
 	if (choice == 0)
 		{
 			printf("Bye!\n");
@@ -40,5 +54,6 @@ int main(void)
 			{
 				printf("Invalid choice\n");
 			}
+	  while (choice != 0);
 	return (0);
 } 
