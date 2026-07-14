5594390bf8de9fa44b0a3d852624decc1789e769
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-14 07:25:42 +0000

    fixed

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -18,15 +18,19 @@ int main(void)
 		printf("0) Quit\n");
 
 		printf("Choice: ");
+
 		if (scanf("%d", &choice) != 1)
 		{
 			int c;
 
 			printf("Invalid choice\n");
 			while ((c = getchar()) != '\n' && c != EOF)
-				choice = -1;
-				continue;
-				}
+			{
+				/* Empty body */
+			}
+			choice = -1;
+			continue;
+		}
 
 		if (choice == 0)
 		{
@@ -36,34 +40,57 @@ int main(void)
 		{
 			int a, b;
 			int c;
+
 			while (1)
 			{
 				printf("A: ");
 				if (scanf("%d", &a) == 1)
+				{
 					break;
+				}
 				printf("Invalid number\n");
 				while ((c = getchar()) != '\n' && c != EOF)
-					while (1)
+				{
+					/* Empty body */
+				}
+			}
+
+			while (1)
 			{
 				printf("B: ");
 				if (scanf("%d", &b) == 1)
+				{
 					break;
-
+				}
 				printf("Invalid number\n");
 				while ((c = getchar()) != '\n' && c != EOF)
-					;
-			} if (choice == 1)
+				{
+					/* Empty body */
+				}
+			}
+
+			if (choice == 1)
+			{
 				printf("Result: %d\n", a + b);
+			}
 			else if (choice == 2)
+			{
 				printf("Result: %d\n", a - b);
+			}
 			else if (choice == 3)
+			{
 				printf("Result: %d\n", a * b);
+			}
 			else if (choice == 4)
 			{
 				if (b == 0)
+				{
 					printf("Error: division by zero\n");
+				}
 				else
+				{
 					printf("Result: %g\n", (double)a / b);
+				}
 			}
 		}
 		else if (choice < 0 || choice > 4)
