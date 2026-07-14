be668eaf5349e079dc7ab27791294988c3ad0ab4
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-14 06:46:09 +0000

    fixed

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Displays a calculator menu and handles the quit option
+ * main - Displays a calculator menu and handles choices
  *
  * Return: Always 0
  */
@@ -9,21 +9,29 @@ int main(void)
 {
 	int choice = -1;
 
-	printf("Simple Calculator\n");
-	printf("1) Add\n");
-	printf("2) Subtract\n");
-	printf("3) Multiply\n");
-	printf("4) Divide\n");
-	printf("0) Quit\n");
+	do { /* 1. بداية اللوب */
+		printf("Simple Calculator\n");
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
 
-	printf("Choice: ");
-	if (scanf("%d", &choice) == 1)
-	{
-		if (choice == 0)
-		{
-			printf("Bye!\n");
-		}
-	}
+		printf("Choice: ");
+
+		if (scanf("%d", &choice) == 1)
+		{ /* 2. بداية قوس الـ scanf */
+			if (choice == 0)
+			{
+				printf("Bye!\n");
+			}
+			else if (choice < 0 || choice > 4)
+			{
+				printf("Invalid choice\n");
+			}
+		} /* 3. إغلاق قوس الـ scanf هنا بعد ما خلصنا الشروط */
+
+	} while (choice != 0); /* 4. نهاية اللوب */
 
 	return (0);
 }
