f62cfb05901b3f45223641b5b0cec47c4d039653
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-14 06:57:21 +0000

    fixed

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -7,31 +7,38 @@
  */
 int main(void)
 {
-	int choice = -1;
-
-	do { /* 1. بداية اللوب */
-		printf("Simple Calculator\n");
-		printf("1) Add\n");
-		printf("2) Subtract\n");
-		printf("3) Multiply\n");
-		printf("4) Divide\n");
-		printf("0) Quit\n");
-
-		printf("Choice: ");
-
-		if (scanf("%d", &choice) == 1)
-		{ /* 2. بداية قوس الـ scanf */
-			if (choice == 0)
+	if (choice == 0)
+		{
+			printf("Bye!\n");
+		}
+			
+	  else if (choice == 1)
 			{
-				printf("Bye!\n");
+				int a, b;
+
+				printf("A: ");
+				scanf("%d", &a);
+
+				printf("B: ");
+				scanf("%d", &b);
+
+				printf("Result: %d\n", a + b);
 			}
-			else if (choice < 0 || choice > 4)
+	  else if (choice == 2)
+			{
+				int a, b;
+
+				printf("A: ");
+				scanf("%d", &a);
+
+				printf("B: ");
+				scanf("%d", &b);
+
+				printf("Result: %d\n", a - b);
+			}
+	  else if (choice < 0 || choice > 4)
 			{
 				printf("Invalid choice\n");
 			}
-		} /* 3. إغلاق قوس الـ scanf هنا بعد ما خلصنا الشروط */
-
-	} while (choice != 0); /* 4. نهاية اللوب */
-
 	return (0);
-}
+} 
