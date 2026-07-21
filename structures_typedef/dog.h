3414014a37f2e04acb872cd2ba1526d3a37e37b0
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-21 10:21:07 +0000

    Add Task 0 - struct dog definition

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog's basic attributes.
+ * @name: Name of the dog (string).
+ * @age: Age of the dog (float).
+ * @owner: Name of the dog's owner (string).
+ *
+ * Description: Header file defining a struct for a dog entity.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
