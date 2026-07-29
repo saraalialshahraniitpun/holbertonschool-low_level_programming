b489730ccbddc6861739f5d137e4979ea15b8520
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-29 06:50:00 +0000

    fixedd

---
+++ function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations based on arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 on success, or exit with status code on error
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	f = get_op_func(argv[2]);
+
+	if (f == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = f(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
