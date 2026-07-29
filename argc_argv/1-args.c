87a15d68c40e0b0a025a84c307301abf8a612ea1
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-07-29 07:03:12 +0000

    Solve Task 1 (argc, argv)

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into the program
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
