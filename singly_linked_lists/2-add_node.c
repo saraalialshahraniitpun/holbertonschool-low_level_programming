c230f118c48f888587b1ff3a3deb98f19b7a4dfe
saraalialshahraniitpun <sara12.shahrani@gmail.com>
2026-08-08 11:00:04 +0000

    Solve Task 2: Add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Double pointer to the head of the list_t list.
+ * @str: String to be added to the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	char *dup_str;
+	unsigned int len = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	if (str != NULL)
+	{
+		dup_str = strdup(str);
+		if (dup_str == NULL)
+			return (NULL);
+		while (str[len] != '\0')
+			len++;
+	}
+	else
+	{
+		dup_str = NULL;
+		len = 0;
+	}
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(dup_str);
+		return (NULL);
+	}
+
+	new->str = dup_str;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
